7087a86190271b5d2612379b82a2962c9e811ae4
aleksandr gulchenko <ogulchenko@contuctor.com>
2021-09-24 12:58:33 +0300

    api date format

--- src/app/pages/seasonality/seasonality.jsx
+++ src/app/pages/seasonality/seasonality.jsx
@@ -8,20 +8,21 @@ import { HeaderContainer, TableContainer } from './components';
 const current = moment().startOf('month');
 const monthes = Array.apply(null, { length: 12 }).map((_, i) => moment(current).add(i, 'month'));
 
+const API_DATE_FORMAT = 'DD-MM-YY';
 export function Seasonality() {
     const { from } = useNavigationParams();
     const { push } = useNamedHistory();
     const getData = useRequest({ endpoint: 'v1/keywords/predictions', namespace: 'forecast', queries: ['from'] });
 
     useEffect(() => {
-        getData({ from: moment(from, 'DD-MM-YY').format('YYYY-MM-DD') || current.format('YYYY-MM-DD') });
+        getData({ from: moment(from, 'DD-MM-YY').format(API_DATE_FORMAT) || current.format(API_DATE_FORMAT) });
         if (!from) {
             push('seasonality', { search: { from: current.format('DD-MM-YY') } });
         }
     }, []);
 
     const handleChange = useCallback((index) => {
-        getData({ from: monthes[index].format('YYYY-MM-DD') });
+        getData({ from: monthes[index].format(API_DATE_FORMAT) });
     }, []);
     return (
         <>
